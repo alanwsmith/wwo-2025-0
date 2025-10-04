0b7e167fa3b46df2f988aeeadf32cce4cd5e92c0
Alan Smith <commit@alanwsmith.com>
2025-10-03 23:39:32 -0400

    Added day 3

--- support/wrapper_builder/build_wrappers.py
+++ support/wrapper_builder/build_wrappers.py
@@ -3,7 +3,7 @@
 from shutil import copy2
 from string import Template
 
-day_range = range(3, 32) 
+day_range = range(4, 32) 
 
 themes = [
     "", "Transparency", "Maps", "Scramble", "Filters", "Remix", "Solids", "Sub-optimal", "Containers", "Blink", "Warning", "Camera", "Battle", "Upside-down", "Doubles", "Unicode", "Transition", "Illumination", "Trading", "Bounce", "Hidden", "Language", "Memory", "Style", "Counter", "Empty", "Texture", "Spell", "Tables", "Surprise", "Deprecated", "Spooky"
@@ -13,18 +13,18 @@ class Maker():
     def output_dir_for_day(self, day):
         return f"../../../wwo-2025-{day}/content/_wrappers"
 
-    def copy_content_files(self):
-        files = [
-                "custom-styles.html",  
-                "base-styles.html", 
-                "bitty-data-send.txt", 
-                "custom-head-tags.html"
-                ]
-        for day in day_range:
-            for source_file in files:
-                dest_path = f"{self.output_dir_for_day(day)}/{source_file}"
-                print(dest_path)
-                copy2(source_file, dest_path)
+    # def copy_content_files(self):
+    #     files = [
+    #             "custom-styles.html",  
+    #             "base-styles.html", 
+    #             "bitty-data-send.txt", 
+    #             "custom-head-tags.html"
+    #             ]
+    #     for day in day_range:
+    #         for source_file in files:
+    #             dest_path = f"{self.output_dir_for_day(day)}/{source_file}"
+    #             print(dest_path)
+    #             copy2(source_file, dest_path)
 
     def generate_wrapper(self, day):
         with open("main.html", "r") as _wrapper_in:
@@ -44,26 +44,34 @@ class Maker():
         with open(output_path, "w") as _out:
             _out.write(content)
 
-
     def output_wrappers(self):
         for day in day_range:
             self.output_wrapper(day)
 
-    def output_dir_for_js(self, day):
-        return f"../../../wwo-2025-{day}/content/components"
+    # def output_dir_for_js(self, day):
+    #     return f"../../../wwo-2025-{day}/content/components"
 
-    def copy_page_js_file(self, day):
-        dest_path = f"{self.output_dir_for_js(day)}/page.js"
-        copy2("page.js", dest_path)
+    # def copy_page_js_file(self, day):
+    #     dest_path = f"{self.output_dir_for_js(day)}/page.js"
+    #     copy2("page.js", dest_path)
 
-    def copy_page_js_files(self):
-        for day in day_range:
-            self.copy_page_js_file(day)
+    # def copy_page_js_files(self):
+    #     for day in day_range:
+    #         self.copy_page_js_file(day)
 
 if __name__ == "__main__":
+
+    from datetime import datetime 
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    print(timestamp)
+
+    print("Making wrappers")
     m = Maker()
 
     # Uncomment to do the files
-    # m.copy_content_files()
     # m.output_wrappers()
+
+
 
